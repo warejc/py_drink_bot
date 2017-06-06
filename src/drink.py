ea91d58b2c351fb0ca71d608d137c4442a706cbc
Julius Ware <jcware8@gmail.com>
2017-06-06 14:18:55 -0400

    Script to make web request

---
+++ src/drink.py
@@ -0,0 +1,12 @@
+import requests
+import configparser
+
+conf = configparser.ConfigParser()
+conf.read('../config.ini')
+
+key = conf.get('Creds', 'key')
+body = {}
+url = 'https://maker.ifttt.com/trigger/drink_water/with/key/{0}'.format(key)
+
+# should add logging here to log the request/response
+req = requests.post(url)
